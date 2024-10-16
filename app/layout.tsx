4176b63dc004d3d97d2f89eaf2b5bf166dce576c
furqan4545 <furqan.ali9500@gmail.com>
2024-10-17 00:18:16 +0900

    launch v2.2

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,21 @@ import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
 import PageHeader from "@/components/page-header";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Feedbek",
+  description:
+    "Build features that your users love. Listening to users feedback has never been this easier before.",
+  icons: {
+    icon: [
+      { url: "/icon_logo.png" },
+      { url: "/icon_logo.png", sizes: "16x16", type: "image/png" },
+      { url: "/icon_logo.png", sizes: "32x32", type: "image/png" },
+    ],
+    apple: [{ url: "/icon_logo.png" }],
+  },
+};
 
 export default function RootLayout({
   children,
