7eb20ad9025d5dbf8e3362da9486fe113c64f222
furqan4545 <furqan.ali9500@gmail.com>
2024-10-20 17:20:26 +0900

    launch v2.0

--- lib/payments.ts
+++ lib/payments.ts
@@ -1,3 +1,5 @@
-export const monthlyPlanId: string = "price_1Q9PtrCEDzUkS6E0Ns8smcwK";
-export const yearlyPlanId: string = "price_1Q9PtrCEDzUkS6E0g94drF56";
+// export const monthlyPlanId: string = "price_1Q9PtrCEDzUkS6E0Ns8smcwK";
+// export const yearlyPlanId: string = "price_1Q9PtrCEDzUkS6E0g94drF56";
+export const monthlyPlanId: string = "price_1QBuLACEDzUkS6E0FEiKnXY4";
+export const yearlyPlanId: string = "price_1QBuM3CEDzUkS6E0T2Q85Vnx";
 export const maxFreeProjects: number = 3;
